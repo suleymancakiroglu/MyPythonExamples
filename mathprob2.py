9a10ea70ba16876e66f2e617f457d634706bc74b
solomoncakir <56886082+suleymancakiroglu@users.noreply.github.com>
2020-12-18 15:05:34 +0300

    Delete mathprob2.py

--- mathprob2.py
+++
@@ -1,6 +0,0 @@
-print("Please enter your weight and height for your Body Mass Index!")
-weight =int(input("Weight :"))
-height =float(input("Height :"))
-
-bmi =weight/(height**2)
-print("Your Body Mass Index is {}".format(bmi))
