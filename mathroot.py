340a3fa714aaddeb5e2fd013ddc6724cbbd1af73
solomoncakir <56886082+suleymancakiroglu@users.noreply.github.com>
2020-12-18 15:06:41 +0300

    Delete mathroot.py

--- mathroot.py
+++
@@ -1,9 +0,0 @@
-a =int(input("a :"))
-b =int(input("b :"))
-c =int(input("c :"))
-
-delta = b**2-4*a*c
-x1 = (-b -delta**0.5)/(2*a)
-x2 = (-b +delta**0.5)/(2*a)
-
-print("First Root :{}\nSecond Root :{}".format(x1,x2))
